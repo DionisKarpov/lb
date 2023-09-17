735698e4bbeaa9afb95bd4ad28066ef115d8ed77
Kateryna Koruma <gifos.kate@gmail.com>
2023-09-17 20:15:01 +0200

    add block developers

--- src/index.js
+++ src/index.js
@@ -176,3 +176,27 @@ async function getEmployeeManagement() {
 }
 
 getEmployeeManagement();
+
+async function getEmployeeDevelopers() {
+	let result = await fetch("https://katerynakoruma.com/api/employees/department/development");
+	let items = await result.json();
+	items.forEach(item => {
+		document.querySelector(".developers-row").innerHTML += `
+		<div class="item-team">
+						<div class="info-box">
+							<div class="box-photo">
+								<img src="${item.picture}" alt="">
+								<a href="mailto:test@gmail.com" class="mail-link"></a>
+							</div>
+							<div class="name">${item.name} ${item.lastname}</div>
+							<div class="name-position">${item.job_title}</div>
+							<div class="description">${item.job_title_description}</div>
+						</div>
+						<a href="" class="linkedin"></a>
+
+					</div>
+		`
+	})
+}
+
+getEmployeeDevelopers();
