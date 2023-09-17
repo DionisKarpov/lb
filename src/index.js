b0106d4bb7d57fb6f4d3f4c192c12129949fc3c7
Kateryna Koruma <gifos.kate@gmail.com>
2023-09-17 18:28:38 +0200

    add core team block

--- src/index.js
+++ src/index.js
@@ -152,3 +152,27 @@ async function getProgressInfo() {
 	})
 }
 getProgressInfo();
+
+async function getEmployeeManagement() {
+	let result = await fetch("https://katerynakoruma.com/api/employees/department/management");
+	let items = await result.json();
+	items.forEach(item => {
+		document.querySelector(".row-core-team").innerHTML += `
+		<div class="item-team">
+						<div class="info-box">
+							<div class="box-photo">
+								<img src="${item.picture}" alt="">
+								<a href="mailto:test@gmail.com" class="mail-link"></a>
+							</div>
+							<div class="name">${item.name} ${item.lastname}</div>
+							<div class="name-position">${item.job_title}</div>
+							<div class="description">${item.job_title_description}</div>
+						</div>
+						<a href="" class="linkedin"></a>
+
+					</div>
+		`
+	})
+}
+
+getEmployeeManagement();
