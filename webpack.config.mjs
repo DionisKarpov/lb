0244c1379fbf6f8ecceb153726d2ec701677d32a
Egor Godovanets <godovanishe@gmail.com>
2024-11-10 17:09:42 +0200

    author mobile is done

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -103,6 +103,10 @@ export default {
 			template: './src/tag-mobile.html',
 			filename: 'tag-mobile.html'
 		}),
+		new HtmlWebpackPlugin({
+			template: './src/author-mobile.html',
+			filename: 'author-mobile.html'
+		}),
 		new MiniCssExtractPlugin()
 	]
 }
