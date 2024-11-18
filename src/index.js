7a56ef87c132a2a38e0c594ff3dfaaa3f9b93892
DionisKarpov <denis@orientedsoft.com>
2024-11-18 21:04:27 +0200

    Updates

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './styles/styles.scss';
+
+[...document.getElementsByClassName('header__menu')].forEach(function (e) {
+  e.addEventListener('click', function (event) {
+    e.classList.toggle('header__menu--opened');
+  });
+});
